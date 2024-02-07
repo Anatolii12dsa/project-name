591e98fe0f251c14ccd46e2108b096548b542051
Anatoly Pozdnyshev <pozdnyshev.anatol@gmail.com>
2024-02-07 12:20:44 +0300

    promo + category

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,6 +8,14 @@ async function bootstrap() {
   const port = parseInt(process.env.PORT);
   const server = process.env.SERVER;
   await app.listen(port, server);
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
